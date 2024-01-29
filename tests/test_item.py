84cdcfa21f6ff188e410ee6745322b1b53d898a9
DarkhanDaurenov <takishevdarkhan@gmail.com>
2024-01-29 16:05:23 +0600

    add src/item.py & tests/test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test_calculate_total_price():
+    item = Item("Тестовый товар", 10.0, 5)
+    assert item.calculate_total_price() == 50.0
+
+
+def test_apply_discount():
+    item = Item("Тестовый товар", 10.0, 5)
+    Item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8.0
